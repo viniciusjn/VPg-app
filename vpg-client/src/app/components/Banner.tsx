55bbceeeb2fa09ec1e97ab5e8d802771f3c3b1d5
viniciusjn <viniciusaqpsjn@gmail.com>
2025-02-28 10:08:28 -0300

    Banner config

--- vpg-client/src/app/components/Banner.tsx
+++ vpg-client/src/app/components/Banner.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import { PlayIcon } from '@heroicons/react/24/solid';
+import Link from "next/link";
 
 export function Banner() {
     return (
         <div className="mb-10 lg:mb-20">
-            <div className="flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[80vh] lg:justify-end lg:pb-12">
+            <div className="flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[76vh] lg:justify-end lg:pb-12">
                 <div className="absolute left-0 top-0 -z-10 flex h-full w-full flex-col bg-[black] overflow-hidden">
 
                     <Image
@@ -31,18 +32,19 @@ export function Banner() {
 
             </div>
 
-            {/* <div className="flex space-x-3">
+            <div className="flex space-x-3">
                 <Link
-                    href={`/watch/${movie.id}`}
+                    href={`https://www.youtube.com/watch?v=QdBZY2fkU-0`}
+                    target="_blank"
                     className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
                     <PlayIcon className='h-6' />
-                    Play
+                    Trailer
                 </Link>
                 <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-500 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
                     <InformationCircleIcon className='h-6' />
                     More Info
                 </button>
-            </div> */}
+            </div>
         </div>
     )
 }
