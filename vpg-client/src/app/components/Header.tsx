5a5516ba30b8abae49376f29d4a43e1251116d4c
viniciusjn <viniciusaqpsjn@gmail.com>
2025-02-28 18:24:09 -0300

    Header config

--- vpg-client/src/app/components/Header.tsx
+++ vpg-client/src/app/components/Header.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header className="fixed top-0 z-50 flex w-full items-center justify-between bg-[#2d0846] p-2 px-4 transition-all lg:px-16 lg:py-4">
-            <Image src="/VPg_logo.png" alt="logo Netflix" width={98} height={98} />
+            <a href="/">
+                <Image src="/VPg_logo.png" alt="logo Netflix" width={98} height={98} className="cursor-pointer"/>
+            </a>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <nav>
                     <ul className="hidden cursor-pointer md:flex md:space-x-4">
