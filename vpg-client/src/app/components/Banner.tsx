b7d2f211e7e8f7f21ebef51164d3f719ffd112f3
viniciusjn <viniciusaqpsjn@gmail.com>
2025-03-01 15:26:08 -0300

    Banner config

--- vpg-client/src/app/components/Banner.tsx
+++ vpg-client/src/app/components/Banner.tsx
@@ -40,10 +40,14 @@ export function Banner() {
                     <PlayIcon className='h-6' />
                     Trailer
                 </Link>
-                <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-500 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-                    <InformationCircleIcon className='h-6' />
-                    More Info
-                </button>
+                <Link
+                    href={`https://www.rockstargames.com/br/VI`}
+                    target="_blank">
+                    <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-500 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
+                        <InformationCircleIcon className='h-6' />
+                        More Info
+                    </button>
+                </Link>
             </div>
         </div>
     )
